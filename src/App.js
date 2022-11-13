46715672338e79431baad0cc2e4a8ee7761b342f
Faiz khan <98544944+hellofaiz@users.noreply.github.com>
2022-11-13 13:33:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="airbnb">
+        <h1 id="heading">The Evolution of  Airbnb's Frontend</h1><br /><br />
+        <h1>@spikebrehm</h1><br />
+        <img  id="image"src="https://tse2.mm.bing.net/th?id=OIP.yqTY33JEGacgFl9EhcHBWQHaE8&pid=Api&P=0" alt="logo" />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
